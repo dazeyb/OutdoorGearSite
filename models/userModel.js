e0ca59a4e6a702c7c7ceb571c0a60bcea06b3c0e
dazeyb <btdazey@gmail.com>
2021-06-01 15:48:29 -0700

    Built out rough models for User and Product

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+/** Schema for Users */
+
+const { Mongoose } = require("mongoose");
+
+const userSchema = new Mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "A name must be provided"],
+        }
+        username: {
+            type: String,
+            required: [true, "A username must be provided"],
+        }
+        password: {
+            type: String,
+            required: [true, "A password must be provided"],
+        }
+        admin: {
+            type: Boolean,
+            required: [true, "A gear type must be provided"],
+        }
+    }
+)
