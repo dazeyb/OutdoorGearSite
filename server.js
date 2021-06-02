987bdc47f5f7019fc61ce00101193134b9d3635c
dazeyb <btdazey@gmail.com>
2021-06-01 18:47:42 -0700

    New ERD draft

--- server.js
+++ server.js
@@ -1,20 +1,59 @@
+/** External Modules */
 const express = require('express');
+
+//      Commented out until used
+// const methodOverride = require("method-override");
+// const session = require("express-session");
+
+// MongoStore will be changed
+// const MongoStore = require("connect-mongo");
+
+
+
+
+/** Instanced Module */
 const app = express();
 
-// DOTENV
+
+/** Configuration */
+//  DOTENV
 require("dotenv").config();
 
-
-
-/** PORT */
+//  PORT
 const PORT = process.env.PORT;
 
+// Set to view EJS
+app.set("view engine", "ejs");
 
 
+
+
+/** Middleware */
+
+//  Body Data
+// app.use(express.urlencoded({ extended: true }));
+
+//  Method Overrride
+// app.use(methodOverride("_method"));
+
+//  Public folder - so that it is accessible across files
+app.use(express.static(__dirname + "/public"));
+
+
+
+
+
+/** Routes */
+
+//  Home Route
 app.get('/', (req, res) => {
     res.send('Root Page');
 });
 
+
+
+
+/** Server Listener */
 app.listen(PORT, ()=>{
     console.log("I am listening");
 });
