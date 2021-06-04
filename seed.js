343fe65655538116993bab91473e9f0b3f8dcf73
dazeyb <btdazey@gmail.com>
2021-06-04 16:13:12 -0700

    Products data seeded, moving to populate page and create show page

---
+++ seed.js
@@ -0,0 +1,13 @@
+const db = require('./models');
+const data = require('./Seed-Data/product.json');
+
+
+db.Product.deleteMany({}, (err, deletedProducts) => {
+    db.Product.create(data.products, (err, seededProducts) => {
+        if (err) console.log(err);
+        
+        // console.log(data.products.length, 'products seeded successfully');
+        
+        process.exit();
+    });
+});
