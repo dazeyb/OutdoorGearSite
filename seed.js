50325d10b615596bf78abfe6d591463c93b33b52
dazeyb <btdazey@gmail.com>
2021-06-07 16:56:44 -0700

    EOD commit

--- seed.js
+++ seed.js
@@ -3,21 +3,30 @@ const data = require('./Seed-Data/product.json');
 
 
     // Reseeds data into database so the website can be demoed 
-const run = async () => {
+// const run = async () => {
 
-    // Removes all products
-    await db.Product.deleteMany({}, (err, deletedProducts) => {
-    db.Product.create(data.products, (err, seededProducts) => {
-        if (err) return console.log(err);
-        
-        console.log(data.products.length, 'Products seeded successfully');
-        
-        process.exit();
-    });
-});
-};
+//     try {
 
-run ();
+//     // Removes all products
+//     await db.Product.deleteMany({});
+
+//     // Creates all products
+//     const createdAuthors = await db.Product.insertMany(products) => {
+//         if (err) return console.log(err);
+        
+//         console.log(data.products.length, 'Products seeded successfully');
+        
+//         process.exit();
+//     } catch (err) {
+//         console.log(err)
+//         process.exit();
+//     }
+//     }
+//     });
+// });
+// };
+
+// run ();
 
 // await db.Store.deleteMany({}, (err, deletedStores) => {
 //     db.Store.create(data.stores, (err, seededStores) => {
@@ -30,4 +39,35 @@ run ();
 // });
 
 
+// Seed stores first, manually add store IDS into names
+ 
+// Then seed products
+// **** Early attempt to run seeded file 
+const run = async () => {
+
+    try {
+    // Removes all products
+    await db.Product.deleteMany({}, (err, deletedProducts) => {
+
+    // Creates all products
+    db.Product.create(data.products, (err, seededProducts) => {
+        if (err) return console.log(err);
+        
+        console.log(data.products.length, 'Products seeded successfully');
+        
+        process.exit();
+    } catch (err) {
+        console.log(err)
+        process.exit();
+    }
+    }
+    });
+});
+};
+
+run ();
+
+
 //  Command to populate db with data: node seed.js
+
+
