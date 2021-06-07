ec3195fb46ffbae770fc9c58ae7b25e3e6b434d6
dazeyb <btdazey@gmail.com>
2021-06-07 14:12:49 -0700

    Commit prior to linking stores - products

--- seed.js
+++ seed.js
@@ -4,13 +4,24 @@ const data = require('./Seed-Data/product.json');
 
 db.Product.deleteMany({}, (err, deletedProducts) => {
     db.Product.create(data.products, (err, seededProducts) => {
-        if (err) console.log(err);
+        if (err) return console.log(err);
         
-        // console.log(data.products.length, 'products seeded successfully');
+        console.log(data.products.length, 'Products seeded successfully');
         
         process.exit();
     });
 });
+
+
+// db.Store.deleteMany({}, (err, deletedStores) => {
+//     db.Store.create(data.stores, (err, seededStores) => {
+//         if (err) return console.log(err);
+        
+//         console.log(data.stores.length, 'Stores seeded successfully');
+        
+//         process.exit();
+//     });
+// });
 
 
 //  Command to populate db with data: node seed.js
